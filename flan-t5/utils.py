7db441ce34c471af14d67ecf93351550d3117c7c
RohitSaha <r.saha1997@gmail.com>
2023-07-26 13:24:46 -0400

    code formatted using black

--- flan-t5/utils.py
+++ flan-t5/utils.py
@@ -6,39 +6,36 @@ from transformers import AutoTokenizer
 from sklearn.model_selection import train_test_split
 from typing import List, Union
 
-def clean_text(texts: List[Union[str, None]], labels: List[Union[str, None]]) -> pd.DataFrame:
+
+def clean_text(
+    texts: List[Union[str, None]], labels: List[Union[str, None]]
+) -> pd.DataFrame:
     """
     The News Group dataset needs to be preprocessed as it has a lot of
     entries with NULL text and/or NULL labels.
     In this function we simply filter out the NULL entries, and
     return a new dataframe with clean texts and labels.
     """
-    new_texts, new_labels = [], [] 
+    new_texts, new_labels = [], []
     for text, label in zip(texts, labels):
         if isinstance(text, str) and isinstance(label, str):
             new_texts.append(text)
             new_labels.append(label)
     new_ids = [i for i in range(len(new_texts))]
-    df = pd.DataFrame(
-        data={
-            "id" : new_ids,
-            "text" : new_texts,
-            "label" : new_labels
-        }
-    )
+    df = pd.DataFrame(data={"id": new_ids, "text": new_texts, "label": new_labels})
 
-    return df 
+    return df
 
-def get_newsgroup_data(args, tokenizer: AutoTokenizer) -> List[Union[DatasetDict, int, int]]:
+
+def get_newsgroup_data(
+    args, tokenizer: AutoTokenizer
+) -> List[Union[DatasetDict, int, int]]:
     dataset_id = "rungalileo/20_Newsgroups_Fixed"
 
     # Load dataset from the hub
     dataset = load_dataset(dataset_id)
-    
-    train_df = clean_text(
-        dataset["train"]["text"],
-        dataset["train"]["label"]
-    )
+
+    train_df = clean_text(dataset["train"]["text"], dataset["train"]["label"])
 
     # sample n points from train_df
     train_df, _ = train_test_split(
@@ -46,48 +43,37 @@ def get_newsgroup_data(args, tokenizer: AutoTokenizer) -> List[Union[DatasetDict
         train_size=args.train_sample_fraction,
         stratify=train_df["label"],
     )
-    
-    test_df = clean_text(
-        dataset["test"]["text"], dataset["test"]["label"]
-    )
+
+    test_df = clean_text(dataset["test"]["text"], dataset["test"]["label"])
 
     train_dataset = datasets.Dataset.from_pandas(train_df)
     test_dataset = datasets.Dataset.from_pandas(test_df)
-    
-    dataset = DatasetDict(
-        {"train" : train_dataset, "test" : test_dataset}
-    )
+
+    dataset = DatasetDict({"train": train_dataset, "test": test_dataset})
 
     print(f"Train dataset size: {len(dataset['train'])}")
     print(f"Test dataset size: {len(dataset['test'])}")
 
-    tokenized_inputs = concatenate_datasets(
-        [dataset["train"], dataset["test"]]
-    ).map(
+    tokenized_inputs = concatenate_datasets([dataset["train"], dataset["test"]]).map(
         lambda x: tokenizer(x["text"], truncation=True),
         batched=True,
-        remove_columns=["text", "label"]
+        remove_columns=["text", "label"],
     )
-    
-    max_source_length = max(
-        [len(x) for x in tokenized_inputs["input_ids"]]
-    )
+
+    max_source_length = max([len(x) for x in tokenized_inputs["input_ids"]])
     print(f"Max source length: {max_source_length}")
 
-    tokenized_targets = concatenate_datasets(
-        [dataset["train"], dataset["test"]]
-    ).map(
+    tokenized_targets = concatenate_datasets([dataset["train"], dataset["test"]]).map(
         lambda x: tokenizer(x["label"], truncation=True),
         batched=True,
-        remove_columns=["text", "label"]
+        remove_columns=["text", "label"],
     )
-    
-    max_target_length = max(
-        [len(x) for x in tokenized_targets["input_ids"]]
-    )
+
+    max_target_length = max([len(x) for x in tokenized_targets["input_ids"]])
     print(f"Max target length: {max_target_length}")
 
     return dataset, max_source_length, max_target_length
+
 
 def get_samsum_data(tokenizer: AutoTokenizer) -> List[Union[DatasetDict, int, int]]:
     dataset_id = "samsum"
@@ -97,33 +83,22 @@ def get_samsum_data(tokenizer: AutoTokenizer) -> List[Union[DatasetDict, int, in
     print(f"Train dataset size: {len(dataset['train'])}")
     print(f"Test dataset size: {len(dataset['test'])}")
 
-    tokenized_inputs = concatenate_datasets(
-        [dataset["train"], dataset["test"]]
-    ).map(
+    tokenized_inputs = concatenate_datasets([dataset["train"], dataset["test"]]).map(
         lambda x: tokenizer(x["dialogue"], truncation=True),
         batched=True,
-        remove_columns=["dialogue", "summary"]
+        remove_columns=["dialogue", "summary"],
     )
-    
-    max_source_length = max(
-        [len(x) for x in tokenized_inputs["input_ids"]]
-    )
+
+    max_source_length = max([len(x) for x in tokenized_inputs["input_ids"]])
     print(f"Max source length: {max_source_length}")
 
-    tokenized_targets = concatenate_datasets(
-        [dataset["train"], dataset["test"]]
-    ).map(
+    tokenized_targets = concatenate_datasets([dataset["train"], dataset["test"]]).map(
         lambda x: tokenizer(x["summary"], truncation=True),
         batched=True,
-        remove_columns=["dialogue", "summary"]
+        remove_columns=["dialogue", "summary"],
     )
 
-    max_target_length = max(
-        [len(x) for x in tokenized_targets["input_ids"]]
-    )
+    max_target_length = max([len(x) for x in tokenized_targets["input_ids"]])
     print(f"Max target length: {max_target_length}")
 
     return dataset, max_source_length, max_target_length
-
-
-
