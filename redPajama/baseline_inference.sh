2543a51c07d4f231582aba64835f6546a5fd555f
RohitSaha <r.saha1997@gmail.com>
2023-08-17 15:52:35 +0000

    added redpajama scripts

---
+++ redPajama/baseline_inference.sh
@@ -0,0 +1,2 @@
+python redpajama_baseline_inference.py --pretrained_ckpt togethercomputer/RedPajama-INCITE-7B-Instruct --task_type classification --prompt_type zero-shot & wait
+python redpajama_baseline_inference.py --pretrained_ckpt togethercomputer/RedPajama-INCITE-7B-Instruct --task_type classification --prompt_type few-shot 
