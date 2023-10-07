c6aa5cea838a1c6770367bcceeb45b76ba216efd
Ubuntu <ubuntu@ip-172-31-55-189.ec2.internal>
2023-10-07 12:00:38 +0000

    rename model to mpt

--- mosaic-mpt/run_lora.sh
+++ mosaic-mpt/run_lora.sh
@@ -5,7 +5,7 @@ dropout=(0.1 0.2 0.5)
 for (( epoch=0; epoch<6; epoch=epoch+1 )) do
 	for ((r=0; r<4; r=r+1 )) do
 		for (( d=0; d<3; d=d+1 )) do
-			python llama2_summarization.py --lora_r ${lora_r[$r]} --epochs ${epochs[$epoch]} --dropout ${dropout[$d]} & wait
+			python mpt_summarization.py --lora_r ${lora_r[$r]} --epochs ${epochs[$epoch]} --dropout ${dropout[$d]} & wait
 		done
 	done
 done
