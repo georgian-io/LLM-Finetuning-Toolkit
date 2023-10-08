ab279f67629862125a3b40497232abcb4edc00e0
Ubuntu <ubuntu@ip-172-31-58-231.ec2.internal>
2023-10-08 00:00:24 +0000

    open finetune files

---
+++ openai/custom_model_inference.sh
@@ -0,0 +1,3 @@
+python jurassic_baseline_inference.py --prompt_type fine-tuned --task_type summarization --model_type j2-large --custom_model j2-light-samsum & wait
+python jurassic_baseline_inference.py --prompt_type fine-tuned --task_type summarization --model_type j2-grande --custom_model j2-mid-samsum & wait
+python jurassic_baseline_inference.py --prompt_type fine-tuned --task_type summarization --model_type j2-jumbo --custom_model j2-ultra-samsum
