46a76f1c58fa217120b6f91c64714bd3cc5a7bbf
Ubuntu <ubuntu@ip-172-31-55-206.ec2.internal>
2023-10-08 16:59:36 +0000

    update flash attention dependency

--- mistral/mistral_classification_inference.py
+++ mistral/mistral_classification_inference.py
@@ -34,7 +34,6 @@ def main(args):
         low_cpu_mem_usage=True,
         torch_dtype=torch.float16,
         load_in_4bit=True,
-        use_flash_attention_2=args.use_flash_attention,
     )
 
     model.eval()
@@ -63,6 +62,7 @@ def main(args):
                     outputs.detach().cpu().numpy(), skip_special_tokens=True
                 )[0]
                 result = result[len(instruct) :]
+                print(result)
             except:
                 result = ""
                 oom_examples.append(input_ids.shape[-1])
@@ -96,7 +96,6 @@ if __name__ == "__main__":
         "--experiment_dir",
         default="experiments/classification-sampleFraction-0.1_epochs-5_rank-8_dropout-0.1",
     )
-    parser.add_argument('--use_flash_attention', action=argparse.BooleanOptionalAction)
 
     args = parser.parse_args()
     main(args)
