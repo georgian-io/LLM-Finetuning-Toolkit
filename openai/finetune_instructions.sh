82a904125cddc105b4fcc5bbac8f0a70928f2987
Ubuntu <ubuntu@ip-172-31-58-231.ec2.internal>
2023-10-08 14:51:02 +0000

    update instruction

--- openai/finetune_instructions.sh
+++ openai/finetune_instructions.sh
@@ -59,5 +59,8 @@ python gpt_finetune.py --job_type infer_finetuned_model --task_type classificati
 python gpt_finetune.py --job_type infer_finetuned_model --task_type classification --model_id ft:gpt-3.5-turbo-0613:georgian::87EoCdcG
 
 # 0.5
+python gpt_finetune.py --job_type infer_finetuned_model --task_type classification --model_id ft:gpt-3.5-turbo-0613:georgian::87FzhTQt
 
 # 0.99
+python gpt_finetune.py --job_type infer_finetuned_model --task_type classification --model_id ft:gpt-3.5-turbo-0613:georgian::87GsKvdl
+
