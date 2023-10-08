c7a3c9c5d1469d5f809c3488adc67253eaf3b535
Ubuntu <ubuntu@ip-172-31-58-231.ec2.internal>
2023-10-08 15:22:23 +0000

    update readme

--- openai/prompts.py
+++ openai/prompts.py
@@ -86,6 +86,7 @@ def get_newsgroup_data_for_ft(train_sample_fraction=0.99):
         train_df,
         train_size=train_sample_fraction,
         stratify=train_df["label"],
+        random_state=42,
     )
     train_data = train_df["text"]
     train_labels = train_df["label"]
