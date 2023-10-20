aa9a3dcef1110e908a246149f01e69bf3f14fd0e
mariia.ponomarenko@georgian.io <mariia.ponomarenko@georgian.io>
2023-10-19 22:12:04 -0400

    added calculating metrics with deviation (+-)

--- inference/automated_benchmark/process_benchmark_data.py
+++ inference/automated_benchmark/process_benchmark_data.py
@@ -1,5 +1,6 @@
 import csv
 import os
+import numpy as np
 from enum_types import Server, Task
 from constants import MICROSECONDS_LENGTH, MILLISECONDS_LENGTH, SECONDS_LENGTH, MINUTES_LENGTH
 from constants import NUMBER_OF_MICROSEC_IN_SECOND, NUMBER_OF_MS_IN_SECOND, NUMBER_OF_SECONDS_IN_MINUTE
@@ -13,16 +14,18 @@ def calculate_num_of_tokens(task, server):
     actual_output_tokens = 0
 
     if task == Task.CLASSIFICATION.value:
+        actual_output_tokens = expected_output_tokens_class
         if server == Server.RAY.value:
-            actual_output_tokens = input_tokens + expected_output_tokens_class
+            actual_output_tokens += input_tokens
     else:
+        actual_output_tokens = expected_output_tokens_summ
         if server == Server.RAY.value:
-            actual_output_tokens = input_tokens + expected_output_tokens_summ
+            actual_output_tokens += input_tokens
 
     return input_tokens, actual_output_tokens
 
 def save_data_for_final_table(csv_file_path, task, data, instance_cost):
-    headers = ["hardware", "server", "rps", "latency", "throughput", "duration", 
+    headers = ["hardware", "server", "rps", "latency_with_deviation", "throughput_with_deviation", "duration_with_deviation", 
                "input_tokens", "output_tokens", "cost"]
     input_tokens, output_tokens = calculate_num_of_tokens(task, data[1])
     number_of_seconds = 3600
@@ -76,35 +79,42 @@ def get_metrics(huggingface_rep: str, task: str, hardware: str, server: str, ins
 
                     total_request = int(raw[pos_of_total_request_value][:-1])
                     if result_dict.get(total_request) is None:
-                        result_dict[total_request] = {'latency': 0, 
-                                                      'throughput': float(raw[pos_of_throughput_value]),
+                        result_dict[total_request] = {'latency': [], 
+                                                      'throughput': [convert_to_seconds(raw[pos_of_throughput_value])],
                                                       'count': 1, 
-                                                      'duration': 0}
+                                                      'duration': []}
                     else:
                         result_dict[total_request]['count'] += 1
-                        result_dict[total_request]['throughput'] += float(raw[pos_of_throughput_value])
+                        result_dict[total_request]['throughput'].append(convert_to_seconds(raw[pos_of_throughput_value]))
                     max_total_request = total_request
                 if raw[0] == 'Duration':
                     pos_of_duration_value = 4
-                    result_dict[max_total_request]['duration'] += convert_to_seconds(raw[pos_of_duration_value])
+                    result_dict[max_total_request]['duration'].append(convert_to_seconds(raw[pos_of_duration_value]))
                 if raw[0] == 'Latencies':
                     pos_of_latency_value = 11
-                    result_dict[max_total_request]['latency'] += convert_to_seconds(raw[pos_of_latency_value])
+                    result_dict[max_total_request]['latency'].append(convert_to_seconds(raw[pos_of_latency_value]))
         
         keys_to_modify = ['latency', 'duration', 'throughput']
 
         for num_req in result_dict.keys():
             for key in keys_to_modify:
-                result_dict[num_req][key] = result_dict[num_req][key] / result_dict[num_req]['count']
+                mean_value = np.mean(result_dict[num_req][key])
+                std_deviation = np.std(result_dict[num_req][key])
+
+                formatted_mean = "{:.3f}".format(mean_value)
+                formatted_std_dev = "{:.3f}".format(std_deviation)
+
+                result_dict[num_req][f"{key}_with_deviation"] = f"{formatted_mean}±{formatted_std_dev}"
+                result_dict[num_req][key] = mean_value
 
         requests = list(result_dict.keys())
         latencies = [result_dict[num_req]['latency'] for num_req in result_dict]
         throughputs = [result_dict[num_req]['throughput'] for num_req in result_dict]
         durations = [result_dict[num_req]['duration'] for num_req in result_dict]
 
-        save_data_for_final_table(path_processed, task, [hardware, server, max_total_request, result_dict[max_total_request]['latency'], 
-                                                        result_dict[max_total_request]['throughput'], 
-                                                        result_dict[max_total_request]['duration']], instance_cost)
+        save_data_for_final_table(path_processed, task, [hardware, server, max_total_request, result_dict[max_total_request]['latency_with_deviation'], 
+                                                        result_dict[max_total_request]['throughput_with_deviation'], 
+                                                        result_dict[max_total_request]['duration_with_deviation']], instance_cost)
         save_data_for_final_plot(path_plot, requests, latencies, throughputs, durations)
 
 if __name__ == '__main__':
