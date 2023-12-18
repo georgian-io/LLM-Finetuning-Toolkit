0bfcfb20421831a43a68ba622ba7246316bcb227
Benjamin Ye <benjamin.ye@georgian.io>
2023-12-18 18:28:48 -0500

    refactoring file and print operations into modules

--- toolkit/src/data/dataset_generator.py
+++ toolkit/src/data/dataset_generator.py
@@ -1,10 +1,17 @@
+import os
+from os.path import join, exists
 from functools import partial
 from typing import Tuple
+import pickle
 
 import re
 from datasets import Dataset
+from rich.console import Console
+from rich.layout import Layout
+from rich.panel import Panel
 
 from src.data.ingestor import Ingestor, get_ingestor
+from toolkit.src.utils.rich_print_utils import inject_example_to_rich_layout
 
 
 class DatasetGenerator:
@@ -15,8 +22,8 @@ class DatasetGenerator:
         prompt: str,
         prompt_stub: str,
         test_size: float,
-        train_size: float
-        # stratify_by: str
+        train_size: float,
+        console: Console,
     ):
         self.ingestor: Ingestor = get_ingestor(type)
         self.ingestor: Ingestor = self.ingestor(path)
@@ -26,9 +33,9 @@ class DatasetGenerator:
         self.prompt_stub: str = prompt_stub
         self.test_size: float = test_size
         self.train_size: float = train_size
-        # self.stratify_by: str = stratify_by
         self.train_columns: list = self._get_train_columns()
         self.test_column: str = self._get_test_column()
+        self.console: Console = console
 
     def _get_train_columns(self):
         pattern = r"\{([^}]*)\}"
@@ -39,7 +46,12 @@ class DatasetGenerator:
         return re.findall(pattern, self.prompt_stub)[0]
 
     def _train_test_split(self):
-        self.dataset = self.dataset.train_test_split(test_size=self.test_size, train_size=self.train_size)
+        self.dataset = self.dataset.train_test_split(
+            test_size=self.test_size, train_size=self.train_size
+        )
+        self.console.print(f"Post-Split data size:")
+        self.console.print(f"Train: {len(self.dataset['train'])}")
+        self.console.print(f"Test: {len(self.dataset['test'])}")
 
     def _format_one_prompt(self, example, is_test: bool = False):
         train_mapping = {var_name: example[var_name] for var_name in self.train_columns}
@@ -52,14 +64,63 @@ class DatasetGenerator:
         return example
 
     def _format_prompts(self):
-        self.dataset["train"] = self.dataset["train"].map(
-            partial(self._format_one_prompt, is_test=False)
-        )
-        self.dataset["test"] = self.dataset["test"].map(
-            partial(self._format_one_prompt, is_test=True)
-        )
+        with self.console.status("Injecting columns into Prompt...", spinner="monkey"):
+            self.dataset["train"] = self.dataset["train"].map(
+                partial(self._format_one_prompt, is_test=False)
+            )
+            self.dataset["test"] = self.dataset["test"].map(
+                partial(self._format_one_prompt, is_test=True)
+            )
 
     def get_dataset(self) -> Tuple[Dataset, Dataset]:
         self._train_test_split()
         self._format_prompts()
+
         return self.dataset["train"], self.dataset["test"]
+
+    def save_dataset(self, save_dir: str):
+        self.console.print("Saving dataset...")
+        os.makedirs(save_dir, exist_ok=True)
+        with open(join(save_dir, "train.pkl"), "wb") as f:
+            pickle.dump(self.dataset["train"], f)
+        with open(join(save_dir, "test.pkl"), "wb") as f:
+            pickle.dump(self.dataset["test"], f)
+        self.console.print("Dataset saved!")
+
+    def print_one_example(self):
+        layout = Layout()
+        layout.split_row(
+            Layout(Panel("Train Sample"), name="train"),
+            Layout(
+                Panel("Inference Sample"),
+                name="inference",
+            ),
+        )
+
+        inject_example_to_rich_layout(
+            layout["train"], "Train Example", self.dataset["train"][0]
+        )
+        inject_example_to_rich_layout(
+            layout["inference"], "Inference Example", self.dataset["test"][0]
+        )
+
+        self.console.print(layout)
+
+    def load_dataset_from_pickle(self, save_dir: str, console: Console):
+        console.print(f"Loading formatted dataset from directory {save_dir}")
+        train_path = join(save_dir, "train.pkl")
+        test_path = join(save_dir, "test_pkl")
+
+        if not exists(train_path):
+            raise FileNotFoundError(f"Train set pickle not found at {train_path}")
+        if not exists(test_path):
+            raise FileNotFoundError(f"Test set pickle not found at {test_path}")
+
+        with open(train_path, "rb") as f:
+            train = pickle.load(f)
+            self.dataset["train"] = train
+        with open(test_path, "rb") as f:
+            test = pickle.load(f)
+            self.dataset["test"] = test
+
+        return train, test
--- toolkit/toolkit.py
+++ toolkit/toolkit.py
@@ -1,101 +1,95 @@
 import os
+from os.path import join, exists
+
 import csv
 from threading import Thread
-import pickle
+
+
+from pydantic import ValidationError
+from src.pydantic_models.config_model import Config
 
 import yaml
-from rich.panel import Panel
-from rich.layout import Layout
 from rich.console import Console
 from rich.table import Table
 from rich.live import Live
 from rich.text import Text
 
 import torch
-from peft import LoraConfig, prepare_model_for_kbit_training, get_peft_model, AutoPeftModelForCausalLM
-from transformers import TrainingArguments, TextIteratorStreamer, AutoTokenizer, ProgressCallback
+from peft import (
+    LoraConfig,
+    prepare_model_for_kbit_training,
+    get_peft_model,
+    AutoPeftModelForCausalLM,
+)
+from transformers import (
+    TrainingArguments,
+    TextIteratorStreamer,
+    AutoTokenizer,
+    ProgressCallback,
+)
 from transformers.utils import logging
 from trl import SFTTrainer
 
 from src.data.dataset_generator import DatasetGenerator
 from src.model.model_loader import ModelLoader
-from src.utils.rich_print import inject_example_to_rich_layout
+from src.utils.save_utils import DirectoryHelper
 
 logging.set_verbosity_error()
 
-# TODO: FUTURE BEN - Please refactor this!!!!!!!
-if __name__ == "__main__":
-    with open("./config.yml", "r") as file:
-        config = yaml.safe_load(file)
 
-    output_dir = os.path.join(
-        config["training"].pop("output_dir", "./"),
-        f"model-{config['model']['model_ckpt'].split('/')[-1]}_epochs-{config['training']['num_train_epochs']}_rank-{config['lora']['r']}_neftuneNoise-{config['sft'].get('neftune_noise_alpha', 'None')}",
-    )
- 
+if __name__ == "__main__":
+    config_path = "./config.yml"  # TODO: parameterize this
+
+    # Load YAML config
+    with open(config_path, "r") as file:
+        try:
+            config = yaml.safe_load(file)
+            config = Config(**config)
+        # validate data with pydantic
+        except ValidationError as e:
+            print(e.json())
+
+    dir_helper = DirectoryHelper(config_path, config)
+
+    output_dir = (
+        dir_helper.save_paths.experiment_path
+    )  # TODO: To be deleted after refactoring... temporary glue code lol
+
     console = Console()
 
     # Loading Data -------------------------------
     console.rule("[bold green]Loading Data")
 
-
-    dataset_generator = DatasetGenerator(**config["data"])
+    dataset_generator = DatasetGenerator(console=console, **config.data)
     train_columns = dataset_generator.train_columns
     test_column = dataset_generator.test_column
-    if not os.path.exists(f"{output_dir}/data"):
-        with console.status("Injecting Columns into Prompt...", spinner="monkey"):
-            train, test = dataset_generator.get_dataset()
 
-
-        console.print("Saving dataset...")
-
-        os.makedirs(f"{output_dir}/data", exist_ok=True)
-        with open(f"{output_dir}/data/train.pkl", 'wb') as f:
-            pickle.dump(train, f)
-        with open(f"{output_dir}/data/test.pkl", 'wb') as f:
-            pickle.dump(test, f)
+    dataset_path = dir_helper.save_paths.dataset_path
+    if not exists(dataset_path):
+        train, test = dataset_generator.get_dataset()
+        dataset_generator.save_dataset(dataset_path)
     else:
-        console.print(f"Fine-Tuned Model Found at {output_dir}... Loading Data from directory")
-        with open(f"{output_dir}/data/train.pkl", "rb") as f:
-            train = pickle.load(f)
-        with open(f"{output_dir}/data/test.pkl", "rb") as f:
-            test = pickle.load(f)
+        train, test = dataset_generator.load_dataset_from_pickle(dataset_path)
 
-    console.print(f"Train size: {len(train)}")
-    console.print(f"Test size: {len(test)}")
-    # Print example
-    layout = Layout()
-    layout.split_row(
-        Layout(Panel("Train Sample"), name="train"),
-        Layout(
-            Panel("Inference Sample"),
-            name="inference",
-        ),
-    )
-    inject_example_to_rich_layout(layout["train"], "Train Example", train[0])
-    inject_example_to_rich_layout(layout["inference"], "Inference Example", test[0])
+    dataset_generator.print_one_example()
 
-    console.print(layout)
-
+    # TODO: FUTURE BEN - Please refactor this!!!!!!!
+    # TODO: Refactored up to here
     # Loading Model -------------------------------
     console.rule("[bold yellow]Loading Model")
 
-    if not os.path.exists(f"{output_dir}/assets"):
-        model, tokenizer = ModelLoader.get_model_and_tokenizer(**config["model"])
+    weights_path = dir_helper.save_paths.weights_path
+    if not exists(weights_path):
+        model, tokenizer = ModelLoader.get_model_and_tokenizer(**config.model])
         console.print(f"{config['model']['model_ckpt']} Loaded :smile:")
     else:
         console.print(f"Fine-Tuned Model Found at {output_dir}... skipping training")
-
 
     # Injecting LoRA -------------------------------
     console.rule("[bold red]Injecting LoRA Adapters")
 
     if not os.path.exists(f"{output_dir}/assets"):
-        config["lora"]["lora_alpha"] = (
-            config["lora"].pop("alpha_factor", 2) * config["lora"]["r"]
-        )
         model.gradient_checkpointing_enable()
-
         peft_config = LoraConfig(**config["lora"])
 
         if config["model"]["quant_4bit"]:
@@ -118,7 +112,7 @@ if __name__ == "__main__":
             output_dir=output_dir,
             logging_dir=logging_dir,
             report_to="none",
-            **config["training"],
+            **config["training"]["training_args"],
         )
 
         progress_callback = ProgressCallback()
@@ -131,7 +125,7 @@ if __name__ == "__main__":
             args=training_args,
             dataset_text_field="formatted_prompt",
             callbacks=[progress_callback],
-            **config["sft"],
+            **config["training"]["sft_args"],
         )
 
         with console.status("Training...", spinner="runner"):
@@ -155,7 +149,7 @@ if __name__ == "__main__":
         low_cpu_mem_usage=True,
         torch_dtype=torch.float16,
         device_map="auto",
-        )
+    )
     model = model.merge_and_unload()
     console.print("Done Merging")
 
@@ -165,9 +159,10 @@ if __name__ == "__main__":
     oom_examples = []
     prompts, labels = test["formatted_prompt"], test[test_column]
 
-
     for idx, (prompt, label) in enumerate(zip(prompts, labels)):
-        inf_table = Table(title=f"Inference Results {idx+1}/{len(prompts)}", show_lines=True)
+        inf_table = Table(
+            title=f"Inference Results {idx+1}/{len(prompts)}", show_lines=True
+        )
         inf_table.add_column("prompt")
         inf_table.add_column("ground truth")
         inf_table.add_row(prompt, label)
@@ -175,14 +170,13 @@ if __name__ == "__main__":
 
         input_ids = tokenizer(
             prompt, return_tensors="pt", truncation=True
-        ).input_ids.cuda(
-        )
+        ).input_ids.cuda()
 
-        streamer = TextIteratorStreamer(tokenizer, skip_prompt=True, decode_kwargs={"skip_special_tokens":True})
-        generation_kwargs = dict( 
-            input_ids=input_ids,
-            streamer=streamer,
-            **config["inference"]
+        streamer = TextIteratorStreamer(
+            tokenizer, skip_prompt=True, decode_kwargs={"skip_special_tokens": True}
+        )
+        generation_kwargs = dict(
+            input_ids=input_ids, streamer=streamer, **config["inference"]
         )
         thread = Thread(target=model.generate, kwargs=generation_kwargs)
         thread.start()
@@ -195,11 +189,11 @@ if __name__ == "__main__":
                 live.update(result)
 
         results.append((prompt, label, str(result)))
-    
+
     console.print("saving inference results...")
 
     header = ["Prompt", "Ground Truth", "Predicted"]
-    with open(f"{output_dir}/output.csv", 'w', newline='') as f:
+    with open(f"{output_dir}/output.csv", "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerow(header)
         for row in results:
