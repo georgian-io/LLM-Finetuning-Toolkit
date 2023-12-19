3e470a9b9d78b2161a8ca9da8757847941858969
Benjamin Ye <benjamin.ye@georgian.io>
2023-12-19 14:48:40 -0500

    refactoring out all training/inference logic

--- toolkit/src/model/model_loader.py
+++ toolkit/src/model/model_loader.py
@@ -1,48 +1,143 @@
+from os.path import join, exists
+
 import torch
 
+from datasets import Dataset
 from transformers import (
     AutoTokenizer,
     AutoModelForCausalLM,
     BitsAndBytesConfig,
+    TrainingArguments,
+    AutoTokenizer,
+    ProgressCallback,
 )
+from peft import (
+    prepare_model_for_kbit_training,
+    get_peft_model,
+    AutoPeftModelForCausalLM,
+    LoraConfig,
+)
+from trl import SFTTrainer
+from rich.console import Console
+
+
+from src.pydantic_models.config_model import Config
+from src.utils.save_utils import DirectoryHelper
 
 
 class ModelLoader:
-    @classmethod
-    def get_model_and_tokenizer(cls, model_ckpt: str, quant_4bit: bool = True):
-        model = cls._get_model(model_ckpt, quant_4bit)
-        tokenizer = cls._get_tokenizer(model_ckpt)
+    def __init__(
+        self, config: Config, console: Console, directory_helper: DirectoryHelper
+    ):
+        self._model_config = config.model
+        self._training_args = config.training.training_arguments
+        self._sft_args = config.training.sft
+        self._lora_config = LoraConfig(**config.lora)
+        self._console: Console = console
+        self._directory_helper = directory_helper
+        self._weights_path = self._directory_helper.save_paths.weights
 
-        return model, tokenizer
+        self.model = None
+        self.tokenizer = None
 
-    @classmethod
-    def _get_model(cls, model_ckpt: str, quant_4bit: bool = True):
-        bnb_config = (
-            BitsAndBytesConfig(
-                load_in_4bit=True,
-                bnb_4bit_use_double_quant=True,
-                bnb_quant_4bit_type="nf4",
-                bnb_4bit_compute_dtype=torch.float16,
-            )
-            if quant_4bit
-            else None
-        )
+    def load_model_and_tokenizer(self):
+        self._console.print(f"Loading {self._model_config.model_ckpt}...")
+        model = self._get_model()
+        tokenizer = self._get_tokenizer()
+        self._console.print(f"{self._model_config.model_ckpt} Loaded :smile:")
 
+        self.model = model
+        self.tokenizer = tokenizer
+
+    def _get_model(self):
         model = AutoModelForCausalLM.from_pretrained(
-            model_ckpt,
-            quantization_config=bnb_config,
+            self._model_config.model_ckpt,
+            quantization_config=BitsAndBytesConfig(self._model_config.bitsandbytes),
             use_cache=False,
-            device_map="auto",
+            device_map=self._model_config.device_map,
         )
 
         model.config.pretraining_tp = 1
 
         return model
 
-    @classmethod
-    def _get_tokenizer(cls, model_ckpt: str):
-        tokenizer = AutoTokenizer.from_pretrained(model_ckpt, device_map="auto")
+    def _get_tokenizer(self):
+        tokenizer = AutoTokenizer.from_pretrained(
+            self._model_config.model_ckpt, self._model_config.device_map
+        )
         tokenizer.pad_token = tokenizer.eos_token
         tokenizer.padding_side = "right"
 
         return tokenizer
+
+    def inject_lora(self):
+        self._console.print(f"Injecting Lora Modules")
+
+        self.model.gradient_checkpointing_enable()
+        self.model = prepare_model_for_kbit_training(self.model)
+        self.model = get_peft_model(self.model, self._lora_config)
+
+        self._console.print(f"LoRA Modules Injected!")
+
+    def train(self, train_dataset: Dataset):
+        logging_dir = join(self._weights_path, "/logs")
+        training_args = TrainingArguments(
+            output_dir=self._weights_path,
+            logging_dir=logging_dir,
+            report_to="none",
+            **self._training_args,
+        )
+
+        progress_callback = ProgressCallback()
+
+        trainer = SFTTrainer(
+            model=self.model,
+            train_dataset=train_dataset,
+            peft_config=self._lora_config,
+            tokenizer=self.tokenizer,
+            packing=True,
+            args=training_args,
+            dataset_text_field="formatted_prompt",  # TODO: maybe move consts to a dedicated folder
+            callbacks=[progress_callback],
+            **self._sft_args,
+        )
+
+        with self._console.status("Training...", spinner="runner"):
+            trainer_stats = trainer.train()
+        self._console.print(f"Training Complete")
+
+        trainer.model.save_pretrained(self._weights_path)
+        self.tokenizer.save_pretrained(self._weights_path)
+
+        self._console.print(f"Run saved at {self._weights_path}")
+
+    def load_and_merge_from_saved(self):
+        # purge VRAM
+        del self.model
+        del self.tokenizer
+        torch.cuda.empty_cache()
+
+        # Load from path
+        self._console.print("Merging Adapter Weights...")
+
+        dtype = (
+            torch.float16
+            if self._training_args.fp16
+            else torch.bfloat16
+            if self._training_args.bf16
+            else torch.float32
+        )
+
+        self.model = AutoPeftModelForCausalLM.from_pretrained(
+            self._weights_path,
+            low_cpu_mem_usage=True,
+            torch_dtype=dtype,
+            device_map=self._model_config.device_map,
+        )
+
+        self.model = self.model.merge_and_unload()
+        self._console.print("Done Merging")
+
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            self._weights_path, device_map=self._model_config.device_map
+        )
--- toolkit/toolkit.py
+++ toolkit/toolkit.py
@@ -1,37 +1,15 @@
-import os
 from os.path import join, exists
+import yaml
 
-import csv
-from threading import Thread
+from rich.console import Console
 
+from transformers.utils import logging
 
 from pydantic import ValidationError
+
 from src.pydantic_models.config_model import Config
-
-import yaml
-from rich.console import Console
-from rich.table import Table
-from rich.live import Live
-from rich.text import Text
-
-import torch
-from peft import (
-    LoraConfig,
-    prepare_model_for_kbit_training,
-    get_peft_model,
-    AutoPeftModelForCausalLM,
-)
-from transformers import (
-    TrainingArguments,
-    TextIteratorStreamer,
-    AutoTokenizer,
-    ProgressCallback,
-)
-from transformers.utils import logging
-from trl import SFTTrainer
-
 from src.data.dataset_generator import DatasetGenerator
-from src.model.model_loader import ModelLoader
+from src.model.model_loader import ModelLoader, InferenceRunner
 from src.utils.save_utils import DirectoryHelper
 
 logging.set_verbosity_error()
@@ -51,10 +29,6 @@ if __name__ == "__main__":
 
     dir_helper = DirectoryHelper(config_path, config)
 
-    output_dir = (
-        dir_helper.save_paths.experiment_path
-    )  # TODO: To be deleted after refactoring... temporary glue code lol
-
     console = Console()
 
     # Loading Data -------------------------------
@@ -64,7 +38,7 @@ if __name__ == "__main__":
     train_columns = dataset_generator.train_columns
     test_column = dataset_generator.test_column
 
-    dataset_path = dir_helper.save_paths.dataset_path
+    dataset_path = dir_helper.save_paths.dataset
     if not exists(dataset_path):
         train, test = dataset_generator.get_dataset()
         dataset_generator.save_dataset(dataset_path)
@@ -73,128 +47,35 @@ if __name__ == "__main__":
 
     dataset_generator.print_one_example()
 
-    # TODO: FUTURE BEN - Please refactor this!!!!!!!
-    # TODO: Refactored up to here
     # Loading Model -------------------------------
-    console.rule("[bold yellow]Loading Model")
+    console.rule("[bold yellow]Loading Model and Prepare for LoRA")
 
-    weights_path = dir_helper.save_paths.weights_path
+    weights_path = dir_helper.save_paths.weights
+    # TODO: hmmm... refactor these params into a seperate dataclass
+    model_loader = ModelLoader(config, console, dir_helper)
     if not exists(weights_path):
-        model, tokenizer = ModelLoader.get_model_and_tokenizer(**config.model])
-        console.print(f"{config['model']['model_ckpt']} Loaded :smile:")
+        model_loader.load_model_and_tokenizer()
+        model_loader.inject_lora()
     else:
-        console.print(f"Fine-Tuned Model Found at {output_dir}... skipping training")
-
-    # Injecting LoRA -------------------------------
-    console.rule("[bold red]Injecting LoRA Adapters")
-
-    if not os.path.exists(f"{output_dir}/assets"):
-        model.gradient_checkpointing_enable()
-        peft_config = LoraConfig(**config["lora"])
-
-        if config["model"]["quant_4bit"]:
-            model = prepare_model_for_kbit_training(model)
-        model = get_peft_model(model, peft_config)
-
-        console.print(f"LoRA Modules Injected")
-    else:
-        console.print(f"Fine-Tuned Model Found at {output_dir}... skipping training")
+        console.print(f"Fine-Tuned Model Found at {weights_path}... skipping training")
 
     # Training -------------------------------
     console.rule("[bold green]:smiley: Training")
 
-    peft_model_id = f"{output_dir}/assets"
+    if not exists(weights_path):
+        model_loader.train(train)
 
-    if not os.path.exists(f"{output_dir}/assets"):
-        logging_dir = f"{output_dir}/logs"
-
-        training_args = TrainingArguments(
-            output_dir=output_dir,
-            logging_dir=logging_dir,
-            report_to="none",
-            **config["training"]["training_args"],
-        )
-
-        progress_callback = ProgressCallback()
-        trainer = SFTTrainer(
-            model=model,
-            train_dataset=train,
-            peft_config=peft_config,
-            tokenizer=tokenizer,
-            packing=True,
-            args=training_args,
-            dataset_text_field="formatted_prompt",
-            callbacks=[progress_callback],
-            **config["training"]["sft_args"],
-        )
-
-        with console.status("Training...", spinner="runner"):
-            trainer_stats = trainer.train()
-        console.print(f"Training Complete")
-
-        trainer.model.save_pretrained(peft_model_id)
-        tokenizer.save_pretrained(peft_model_id)
-        console.print(f"Run saved at {output_dir}")
-
-    else:
-        console.print(f"Fine-Tuned Model Found at {output_dir}... skipping training")
-
+    # TODO: FUTURE BEN - Please refactor this!!!!!!!
+    # TODO: Refactored up to here
     # Inference -------------------------------
     console.rule("[bold pink]:face_with_monocle: Testing")
+    results_path = dir_helper.save_paths.results
+    results_file_path = join(dir_helper.save_paths.results, "results.csv")
+    if not exists(results_path) or exists(results_file_path):
+        model_loader.load_and_merge_from_saved()
+        model, tokenizer = model_loader.model, model_loader.tokenizer
 
-    console.print("Merging Adapter Weights...")
-    torch.cuda.empty_cache()
-    model = AutoPeftModelForCausalLM.from_pretrained(
-        peft_model_id,
-        low_cpu_mem_usage=True,
-        torch_dtype=torch.float16,
-        device_map="auto",
-    )
-    model = model.merge_and_unload()
-    console.print("Done Merging")
-
-    tokenizer = AutoTokenizer.from_pretrained(peft_model_id, device_map="auto")
-
-    results = []
-    oom_examples = []
-    prompts, labels = test["formatted_prompt"], test[test_column]
-
-    for idx, (prompt, label) in enumerate(zip(prompts, labels)):
-        inf_table = Table(
-            title=f"Inference Results {idx+1}/{len(prompts)}", show_lines=True
-        )
-        inf_table.add_column("prompt")
-        inf_table.add_column("ground truth")
-        inf_table.add_row(prompt, label)
-        console.print(inf_table)
-
-        input_ids = tokenizer(
-            prompt, return_tensors="pt", truncation=True
-        ).input_ids.cuda()
-
-        streamer = TextIteratorStreamer(
-            tokenizer, skip_prompt=True, decode_kwargs={"skip_special_tokens": True}
-        )
-        generation_kwargs = dict(
-            input_ids=input_ids, streamer=streamer, **config["inference"]
-        )
-        thread = Thread(target=model.generate, kwargs=generation_kwargs)
-        thread.start()
-
-        console.print("[bold red]Prediction >")
-        result = Text()
-        with Live(result, refresh_per_second=4, vertical_overflow="visible") as live:
-            for new_text in streamer:
-                result.append(new_text)
-                live.update(result)
-
-        results.append((prompt, label, str(result)))
-
-    console.print("saving inference results...")
-
-    header = ["Prompt", "Ground Truth", "Predicted"]
-    with open(f"{output_dir}/output.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(header)
-        for row in results:
-            writer.writerow(row)
+        # TODO: hmmm... refactor these params into a seperate dataclass
+        inference_runner = InferenceRunner(
+            model, tokenizer, test, test_column, config, console, results_file_path
+        ).run_inference()
