e41973ef67faa8b42a700d3c1f74cb92e5082b23
Benjamin Ye <benjamin.ye@georgian.io>
2024-02-26 10:45:48 -0500

    abstraction definition

---
+++ toolkit/src/finetune/finetune.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod
+from typing import Union, List, Tuple, Dict
+
+
+class Finetune(ABC):
+    @abstractmethod
+    def finetune(self):
+        pass
+
+    @abstractmethod
+    def save_model(self):
+        pass
+
+    @abstractmethod
+    def load_model(self):
+        pass
