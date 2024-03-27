e12727f78247c12f855519113c21738f1512e63a
RohitSaha <r.saha1997@gmail.com>
2024-03-27 10:21:02 -0400

    changes

--- falcon/baseline_inference.sh
+++
@@ -1,4 +0,0 @@
-python falcon_baseline_inference.py --task_type classification --prompt_type zero-shot & wait
-python falcon_baseline_inference.py --task_type classification --prompt_type few-shot & wait
-python falcon_baseline_inference.py --task_type summarization --prompt_type zero-shot & wait
-python falcon_baseline_inference.py --task_type summarization --prompt_type few-shot 
